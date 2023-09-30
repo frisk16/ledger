0f98dbd08be351afe4e92756c141892cb5e9cc39
frisk16 <test-n18@hotmail.co.jp>
2023-09-30 09:30:18 +0900

    edit data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ INSERT IGNORE INTO roles (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT IGNORE INTO roles (id, name) VALUES (2, 'ROLE_GENERAL');
 
 -- usersテーブル
-INSERT IGNORE INTO users (id, role_id, name, email, password, address, phone_number, enabled) VALUES (1, 2, 'test', 'test@co.jp', '$2a$08$lDx6jvFVz3zm.p8R7Dx1Vu84oEj7hNX6rE4/iFI3H0ZDMlJb2ZWie', '東京都 渋谷区', '03-3333-3333', 1);
+INSERT IGNORE INTO users (id, role_id, name, email, password, address, phone_number, enabled) VALUES (1, 1, '管理者', 'test-n18@hotmail.co.jp', '$2a$08$DD.Xs.edaIub6R96/T/CReC8hUxUBquEI7a1FbvSYkIlwNFm/833i', '神奈川県', '080-6651-8190', 1);
