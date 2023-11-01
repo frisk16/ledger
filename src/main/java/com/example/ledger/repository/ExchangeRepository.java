11fc1d0173553a12a61fb881296ef9defbdfc2a0
frisk16 <test-n18@hotmail.co.jp>
2023-11-02 03:49:03 +0900

    drop 3 tables

--- src/main/java/com/example/ledger/repository/ExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ledger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ledger.entity.Exchange;
-
-public interface ExchangeRepository extends JpaRepository<Exchange, Integer> {
-  
-}
--- src/main/java/com/example/ledger/repository/PartsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ledger.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.ledger.entity.Parts;
-
-public interface PartsRepository extends JpaRepository<Parts, Integer> {
-  
-}
