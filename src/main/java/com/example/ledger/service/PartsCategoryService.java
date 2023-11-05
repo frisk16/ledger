79ec0c94ea2286676284ff096202086c122c9018
frisk16 <test-n18@hotmail.co.jp>
2023-11-06 00:26:22 +0900

    change parts category service

--- src/main/java/com/example/ledger/service/PartsCategoryService.java
+++ src/main/java/com/example/ledger/service/PartsCategoryService.java
@@ -37,7 +37,7 @@ public class PartsCategoryService {
   @Transactional
   public void create(PartsCategoryRegisterForm registerForm) {
     MultipartFile imageFile = registerForm.getImage();
-    String imageName = null;
+    String imageName = "";
 
     if(!imageFile.isEmpty()) {
       imageName = imageFile.getOriginalFilename();
