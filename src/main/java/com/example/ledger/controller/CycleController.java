3ecbe96431e67ae20c52afb9e9538b23d8a4b746
frisk16 <test-n18@hotmail.co.jp>
2023-11-19 19:26:22 +0900

    create cycles show page & add not-found-image

--- src/main/java/com/example/ledger/controller/CycleController.java
+++ src/main/java/com/example/ledger/controller/CycleController.java
@@ -3,13 +3,20 @@ package com.example.ledger.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.ledger.entity.Parts;
 import com.example.ledger.entity.PartsCategory;
 import com.example.ledger.repository.PartsCategoryRepository;
+import com.example.ledger.repository.PartsRepository;
 
 @Controller
 @RequestMapping("/cycles")
@@ -19,11 +26,14 @@ public class CycleController {
   private String imageUrl;
 
   private final PartsCategoryRepository categoryRepository;
+  private final PartsRepository partsRepository;
 
   public CycleController(
-    PartsCategoryRepository categoryRepository
+    PartsCategoryRepository categoryRepository,
+    PartsRepository partsRepository
   ) {
     this.categoryRepository = categoryRepository;
+    this.partsRepository = partsRepository;
   }
 
 
@@ -35,6 +45,22 @@ public class CycleController {
     model.addAttribute("imageUrl", this.imageUrl);
 
     return "cycles/index";
+  }
+
+  @GetMapping("/{id}")
+  public String show(
+    @PathVariable(name = "id") Integer id,
+    @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
+    Model model
+  ) {
+    PartsCategory category = this.categoryRepository.getReferenceById(id);
+    Page<Parts> parts = this.partsRepository.findByPartsCategoryOrderByCreatedAtDesc(category, pageable);
+
+    model.addAttribute("category", category);
+    model.addAttribute("parts", parts);
+    model.addAttribute("imageUrl", this.imageUrl);
+
+    return "cycles/show";
   }
 
 }
--- src/main/java/com/example/ledger/repository/PartsRepository.java
+++ src/main/java/com/example/ledger/repository/PartsRepository.java
@@ -1,9 +1,14 @@
 package com.example.ledger.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.ledger.entity.Parts;
+import com.example.ledger.entity.PartsCategory;
 
 public interface PartsRepository extends JpaRepository<Parts, Integer> {
   
+  public Page<Parts> findByPartsCategoryOrderByCreatedAtDesc(PartsCategory category, Pageable pageable); 
+
 }
