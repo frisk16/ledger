1126d66230888e9d7506970f2f7a62ac72acb3f9
Naoto Kaneko <test-n18@hotmail.co.jp>
2024-04-30 21:00:10 +0900

    add test_user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ INSERT IGNORE INTO roles (id, name) VALUES (2, 'ROLE_GENERAL');
 
 -- usersテーブル
 INSERT IGNORE INTO users (id, role_id, name, email, password, address, phone_number, enabled) VALUES (1, 1, '管理者', 'test-n18@hotmail.co.jp', '$2a$08$DD.Xs.edaIub6R96/T/CReC8hUxUBquEI7a1FbvSYkIlwNFm/833i', '神奈川県', '080-6651-8190', 1);
+INSERT IGNORE INTO users (id, role_id, name, email, password, address, phone_number, enabled) VALUES (3, 2, 'Test User', 'test@gmail.com', '$2a$08$KSdI/mVRMi8nAFeuN5egCurmh4uEUzKMtrP67tGqaOTvD9w5uiqNC', '神奈川県', '01-2345-6789', 1);
 
 
 -- noticesテーブル
